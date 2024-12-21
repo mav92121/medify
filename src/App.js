93a86b6761ef19346b70d16e28003508c58a4b5b
mohsinali <mohsinali530121@gmail.com>
2024-12-21 20:09:26 +0530

    mui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
+import { CssBaseline } from "@mui/material";
 
 function App() {
   return (
     <div>
-      <p>app</p>
+      <CssBaseline />
       <Outlet />
     </div>
   );
