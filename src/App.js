8ae1030b984466ac5aa026a39745d672b0dc8e8d
mohsinali <mohsinali530121@gmail.com>
2024-12-21 20:15:35 +0530

    footer added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
 import { CssBaseline } from "@mui/material";
+import Footer from "./components/Footer/Footer";
+import DownloadApp from "./components/Sections/DownloadApp/DownloadApp";
 
 function App() {
   return (
     <div>
       <CssBaseline />
       <Outlet />
+      <DownloadApp />
+      <Footer />
     </div>
   );
 }
